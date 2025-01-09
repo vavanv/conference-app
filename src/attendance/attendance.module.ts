eb7e6a3f4030b9a455c1fcdefd3c021830aa11b8
Vladimir V <vladimir.vorobiev@gmail.com>
2025-01-09 16:18:59 -0500

    remove shit

--- src/attendance/attendance.module.ts
+++
@@ -1,9 +0,0 @@
-import { Module } from '@nestjs/common';
-    import { AttendanceController } from './attendance.controller';
-    import { AttendanceService } from './attendance.service';
-
-    @Module({
-      controllers: [AttendanceController],
-      providers: [AttendanceService]
-    })
-    export class AttendanceModule {}
--- src/presenters/presenters.module.ts
+++
@@ -1,9 +0,0 @@
-import { Module } from '@nestjs/common';
-    import { PresentersController } from './presenters.controller';
-    import { PresentersService } from './presenters.service';
-
-    @Module({
-      controllers: [PresentersController],
-      providers: [PresentersService]
-    })
-    export class PresentersModule {}
